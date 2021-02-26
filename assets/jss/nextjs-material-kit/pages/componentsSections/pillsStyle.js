a8fc314fc3a78bcfc81757664b9204ffa3d2e641
Michael_Peters <michaelpetersiduku@gmail.com>
2021-02-26 03:08:43 +0100

    initial commit

---
+++ assets/jss/nextjs-material-kit/pages/componentsSections/pillsStyle.js
@@ -0,0 +1,16 @@
+import { container, title } from "assets/jss/nextjs-material-kit.js";
+
+const pillsStyle = {
+  section: {
+    padding: "0 0"
+  },
+  container,
+  title: {
+    ...title,
+    marginTop: "0px",
+    minHeight: "100%",
+    textDecoration: "none"
+  }
+};
+
+export default pillsStyle;
---
+++ assets/jss/nextjs-material-kit/pages/landingPage.js
@@ -0,0 +1,36 @@
+import { container, title } from "assets/jss/nextjs-material-kit.js";
+
+const landingPageStyle = {
+  container: {
+    zIndex: "12",
+    color: "#FFFFFF",
+    ...container
+  },
+  title: {
+    ...title,
+    display: "inline-block",
+    position: "relative",
+    marginTop: "30px",
+    minHeight: "32px",
+    color: "#FFFFFF",
+    textDecoration: "none"
+  },
+  subtitle: {
+    fontSize: "1.313rem",
+    maxWidth: "50px",
+    margin: "0 auto 0"
+  },
+  main: {
+    background: "#FFFFFF",
+    position: "relative",
+    zIndex: "3"
+  },
+  mainRaised: {
+    margin: "0",
+    borderRadius: "6px",
+    boxShadow:
+      "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
+  }
+};
+
+export default landingPageStyle;
